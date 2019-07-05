6948977edfe29dfd888d74485b79b51394695f36
marc <marcmaligmat.com>
2019-07-05 19:08:05 +0800

    adding template

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,38 @@
+<header id="header" class="">
+    <div id="header-wrap">
+
+        <!-- Primary Navigation
+        ============================================= -->
+        <nav id="primary-menu" class="style-2 center">
+
+            <div class="container clearfix">
+
+                <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+                <ul>
+                    <li class="current"><a href="#"><div>Home</div></a></li>
+                    <li><a href="#"><div>Features</div></a></li>
+                    <li><a href="#"><div>Categories</div></a></li>
+                    <li><a href="#"><div>Authors</div></a></li>
+                    <li><a href="#"><div>Portfolio</div></a></li>
+                    <li><a href="#"><div>Blog</div></a></li>
+                    <li><a href="#"><div>Contribute</div></a></li>
+                    <li><a href="#"><div>Contact</div></a></li>
+                </ul>
+
+                <!-- Top Search
+                ============================================= -->
+                <div id="top-search">
+                    <a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+                    <form action="search.html" method="get">
+                        <input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+                    </form>
+                </div><!-- #top-search end -->
+
+            </div>
+
+        </nav><!-- #primary-menu end -->
+
+    </div>
+
+</header>

8240cc16813c21c4e95005395b0b3093e9a716f1
marc <marcmaligmat.com>
2019-07-09 18:34:40 +0800

    added image upload

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,22 +21,32 @@ class UsersController extends Controller
         if($request->hasFile('avatar'))
         {
             $user  = Auth::user();
-            // Delete current image before uploading new image
-            if ($user->avatar != 'default.jpg') 
-            {
-                $file = public_path('/storage/uploads/avatars/' . $user->avatar);
-                unlink($file);
-            }
+        //     // Delete current image before uploading new image
+        //     if ($user->avatar != 'default.jpg') 
+        //     {
+        //         $file = public_path('/storage/uploads/avatars/' . $user->avatar);
+        //         unlink($file);
+        //     }
 
+
+        //     $avatar = $request->file('avatar');
+        //     $filename = time() . '.' . $avatar->getClientOriginalExtension();
+        //    // echo public_path('/storage/uploads/avatars/'. $filename);
+        //     Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
+        //     $user->avatar = $filename;
+        //     $user->save();
+
+        //     return redirect('users')->with('success', "Image uploaded successfully.");
 
             $avatar = $request->file('avatar');
-            $filename = time() . '.' . $avatar->getClientOriginalExtension();
-           // echo public_path('/storage/uploads/avatars/'. $filename);
-            Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
-            $user->avatar = $filename;
-            $user->save();
+            $fileNameToStore = time() . '.jpg'; 
 
-            return redirect('users')->with('success', "Image uploaded successfully.");
+            $source = storage_path().'/app/public/uploads/avatars/'.$fileNameToStore;
+            $target = public_path('/uploads/avatars/' . $avatar);
+
+            dd($source, $target);
+
+            Image::make($source)->fit(90, 90)->save($target);
         }
 
     }

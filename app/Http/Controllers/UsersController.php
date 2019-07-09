312ff6027a3db564fa372836459c128140ca48de
marc <marcmaligmat.com>
2019-07-09 18:50:37 +0800

    added image upload

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -21,32 +21,23 @@ class UsersController extends Controller
         if($request->hasFile('avatar'))
         {
             $user  = Auth::user();
-        //     // Delete current image before uploading new image
-        //     if ($user->avatar != 'default.jpg') 
-        //     {
-        //         $file = public_path('/storage/uploads/avatars/' . $user->avatar);
-        //         unlink($file);
-        //     }
+            // Delete current image before uploading new image
+            if ($user->avatar != 'default.jpg') 
+            {
+                $file = public_path('/storage/uploads/avatars/' . $user->avatar);
+                unlink($file);
+            }
 
-
-        //     $avatar = $request->file('avatar');
-        //     $filename = time() . '.' . $avatar->getClientOriginalExtension();
-        //    // echo public_path('/storage/uploads/avatars/'. $filename);
-        //     Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
-        //     $user->avatar = $filename;
-        //     $user->save();
-
-        //     return redirect('users')->with('success', "Image uploaded successfully.");
 
             $avatar = $request->file('avatar');
-            $fileNameToStore = time() . '.jpg'; 
+            $filename = time() . '.' . $avatar->getClientOriginalExtension();
+           // echo public_path('/storage/uploads/avatars/'. $filename);
+            Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
+            $user->avatar = $filename;
+            $user->save();
 
-            $source = storage_path().'/app/public/uploads/avatars/'.$fileNameToStore;
-            $target = public_path('/uploads/avatars/' . $fileNameToStore);
+            return redirect('users')->with('success', "Image uploaded successfully.");
 
-            //dd($source, $target);
-
-            Image::make($source)->resize(90, 90)->save($target);
         }
 
     }

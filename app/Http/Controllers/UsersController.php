a735b3651ec190bdef52a87121bb351e82791182
marc <marcmaligmat.com>
2019-07-09 18:10:11 +0800

    added image upload

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,23 +18,29 @@ class UsersController extends Controller
     {
         //echo $request->get('name');
 
-        if($request->hasFile('avatar'))
-        {
+        if($request->hasFile('avatar')){
             $user  = Auth::user();
+
+            $photo = $request->file('mini');
+            $save_path =public_path('storage/uploads/avatars');
+            chmod($save_path,666);
+            $filename = $save_path.'/'.$request->get('name').'.jpg';
+            Image::make($photo)->resize(90,90)->save($filename);
             // Delete current image before uploading new image
-            if ($user->avatar != 'default.jpg') 
-            {
-                $file = public_path('/storage/uploads/avatars/' . $user->avatar);
-                unlink($file);
-            }
+        //     if ($user->avatar != 'default.jpg'){
+        //         $file = public_path('/storage/uploads/avatars/' . $user->avatar);
+        //         unlink($file);
+        //     } else {
+
+        //     }
 
 
-            $avatar = $request->file('avatar');
-            $filename = time() . '.' . $avatar->getClientOriginalExtension();
-           // echo public_path('/storage/uploads/avatars/'. $filename);
-            Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
-            $user->avatar = $filename;
-            $user->save();
+        //     $avatar = $request->file('avatar');
+        //     $filename = time() . '.' . $avatar->getClientOriginalExtension();
+        //    // echo public_path('/storage/uploads/avatars/'. $filename);
+        //     Image::make($avatar)->resize(90,90)->save( public_path('storage/uploads/avatars/'. $filename));
+        //     $user->avatar = $filename;
+        //     $user->save();
 
             return redirect('users')->with('success', "Image uploaded successfully.");
         }
